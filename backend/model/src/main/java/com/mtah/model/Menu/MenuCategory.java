8b1e7f8484a7f46954863d2d06718c53870908e5
nursh <nsheikhmohammed01@gmail.com>
2020-05-28 00:12:34 -0400

    add MenuItems to MenuCategory. #14.

--- backend/model/src/main/java/com/mtah/model/Menu/MenuCategory.java
+++ backend/model/src/main/java/com/mtah/model/Menu/MenuCategory.java
@@ -4,8 +4,11 @@ import com.mtah.model.Chef;
 import com.mtah.model.NamedEntity;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class MenuCategory extends NamedEntity {
 
     private Chef chef;
+    private List<MenuItem> menuItems;
 }
