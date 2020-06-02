a8c9b214de399f56b687c61d9a77664f50326282
mustaphaattah <mustaphaattah@gmail.com>
2020-06-02 18:10:09 -0400

    created CustomerRepository and CustomerRepository #24 #25

---
+++ backend/persistence/src/main/java/com/mtah/persistence/Repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.mtah.persistence.Repository;
+
+import com.mtah.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
