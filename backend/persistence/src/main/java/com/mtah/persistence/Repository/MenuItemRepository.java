7bb02ce62898e8e572920ea2f6d7160decac27be
nursh <nsheikhmohammed01@gmail.com>
2020-06-02 19:11:25 -0400

    Add MenuItemRepository. Closes #29.

---
+++ backend/persistence/src/main/java/com/mtah/persistence/Repository/MenuItemRepository.java
@@ -0,0 +1,9 @@
+package com.mtah.persistence.Repository;
+
+import com.mtah.model.Menu.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+}
