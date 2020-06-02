9bd097f6141b93553b145d1c6bdd902757de2c52
mustaphaattah <mustaphaattah@gmail.com>
2020-06-02 18:02:16 -0400

    created order repository and order service #20 #21

---
+++ backend/persistence/src/main/java/com/mtah/persistence/Repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.mtah.persistence.Repository;
+
+import com.mtah.model.Order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
