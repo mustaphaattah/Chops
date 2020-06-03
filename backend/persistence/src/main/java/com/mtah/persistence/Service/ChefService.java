435a83418c54218ebc0ed846bfc9f7c4f61191d5
nursh <nsheikhmohammed01@gmail.com>
2020-06-03 18:26:50 -0400

    Add ChefService class. Closes #23.

--- backend/persistence/src/main/java/com/mtah/persistence/Service/ChefService.java
+++ backend/persistence/src/main/java/com/mtah/persistence/Service/ChefService.java
@@ -1,7 +1,37 @@
 package com.mtah.persistence.Service;
 
+import com.mtah.model.Chef;
+import com.mtah.persistence.Repository.ChefRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ChefService {
+public class ChefService implements EntityService<Chef, Long> {
+
+    private final ChefRepository chefRepository;
+
+    public ChefService(ChefRepository cr) {
+        this.chefRepository = cr;
+    }
+
+    @Override
+    public Chef findById(Long id) {
+        return chefRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Chef> findAll() {
+        return chefRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        chefRepository.deleteById(id);
+    }
+
+    @Override
+    public Chef save(Chef chef) {
+        return chefRepository.save(chef);
+    }
 }
