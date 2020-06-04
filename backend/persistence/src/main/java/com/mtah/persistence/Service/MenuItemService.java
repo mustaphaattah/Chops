18b1ae9ee120cd671c85477e63ec1ce835286fa5
nursh <nsheikhmohammed01@gmail.com>
2020-06-04 14:40:57 -0400

    Add MenuItem Service. Closes #28.

---
+++ backend/persistence/src/main/java/com/mtah/persistence/Service/MenuItemService.java
@@ -0,0 +1,37 @@
+package com.mtah.persistence.Service;
+
+import com.mtah.model.Menu.MenuItem;
+import com.mtah.persistence.Repository.MenuItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService implements EntityService<MenuItem, Long> {
+
+    private final MenuItemRepository menuItemRepository;
+
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    @Override
+    public List<MenuItem> findAll() {
+        return menuItemRepository.findAll();
+    }
+
+    @Override
+    public MenuItem findById(Long id) {
+        return menuItemRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        menuItemRepository.deleteById(id);
+    }
+
+    @Override
+    public MenuItem save(MenuItem menuItem) {
+        return menuItemRepository.save(menuItem);
+    }
+}
