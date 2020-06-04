e10e94a97cc15d5994e97057592fa9632e6922ac
nursh <nsheikhmohammed01@gmail.com>
2020-06-04 15:03:49 -0400

    add menuCategoryRepository and MenuCategoryServices. Closes #33, Closes #32.

---
+++ backend/persistence/src/main/java/com/mtah/persistence/Service/MenuCategoryService.java
@@ -0,0 +1,38 @@
+package com.mtah.persistence.Service;
+
+import com.mtah.model.Menu.MenuCategory;
+import com.mtah.persistence.Repository.MenuCategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuCategoryService implements EntityService<MenuCategory, Long> {
+
+    private final MenuCategoryRepository menuCategoryRepository;
+
+
+    public MenuCategoryService(MenuCategoryRepository menuCategoryRepository) {
+        this.menuCategoryRepository = menuCategoryRepository;
+    }
+
+    @Override
+    public List<MenuCategory> findAll() {
+        return menuCategoryRepository.findAll();
+    }
+
+    @Override
+    public MenuCategory findById(Long id) {
+        return menuCategoryRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        menuCategoryRepository.deleteById(id);
+    }
+
+    @Override
+    public MenuCategory save(MenuCategory menuCategory) {
+        return menuCategoryRepository.save(menuCategory);
+    }
+}
