94bc573f9e5df7df3c8040ad8ae38c1bd10d5edb
nursh <nsheikhmohammed01@gmail.com>
2020-06-04 14:53:35 -0400

    Add MenuOptionService. Closes #26, closes #27.

---
+++ backend/persistence/src/main/java/com/mtah/persistence/Service/MenuOptionService.java
@@ -0,0 +1,37 @@
+package com.mtah.persistence.Service;
+
+import com.mtah.model.Menu.MenuOption;
+import com.mtah.persistence.Repository.MenuOptionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuOptionService implements EntityService<MenuOption, Long> {
+
+    private final MenuOptionRepository menuOptionRepository;
+
+    public MenuOptionService(MenuOptionRepository menuOptionRepository) {
+        this.menuOptionRepository = menuOptionRepository;
+    }
+
+    @Override
+    public List<MenuOption> findAll() {
+        return menuOptionRepository.findAll();
+    }
+
+    @Override
+    public MenuOption findById(Long id) {
+        return menuOptionRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        menuOptionRepository.deleteById(id);
+    }
+
+    @Override
+    public MenuOption save(MenuOption menuOption) {
+        return menuOptionRepository.save(menuOption);
+    }
+}
